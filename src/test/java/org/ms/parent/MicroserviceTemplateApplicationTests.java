68cbe4acfe97a2ff22db7fa05ea385529ad362cc
TejasRawat <tejas.rawat6@gmail.com>
2021-07-20 00:15:55 +0530

    first commit

---
+++ src/test/java/org/ms/parent/MicroserviceTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ms.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
